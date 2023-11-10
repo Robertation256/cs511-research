8aeda4ff435a0fd855b0c235279747fa297776e0
Robertation <67719835+Robertation256@users.noreply.github.com>
2023-11-10 10:21:52 -0600

    Update pom

--- cs511-research/src/main/java/org/cs511/Main.java
+++ cs511-research/src/main/java/org/cs511/Main.java
@@ -1,12 +1,16 @@
 package org.cs511;
 
+import org.apache.flink.configuration.Configuration;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.cs511.pipelines.TuplePipeline;
 
 public class Main {
     public static void main(String[] args) {
+        Configuration conf = new Configuration();
+//        conf.setString("rest.port", "8000");
+//        conf.setString("rest.flamegraph.enabled", "true");
 
-        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment(conf);
 
         env = TuplePipeline.create(env);
 
