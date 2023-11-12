9261e2ae04cfcf5c3bf55008843c4a7de8616ef2
owenhhj <juanjuanjks@163.com>
2023-11-11 20:06:24 -0600

    Wrote Avro for low nested level dataset

--- cs511-research/src/main/java/org/cs511/datasource/DummyAvroDataSource.java
+++ cs511-research/src/main/java/org/cs511/datasource/DummyAvroDataSource.java
@@ -1,6 +1,9 @@
 package org.cs511.datasource;
 
 
+import java.io.FileReader;
+import java.util.Iterator;
+import org.json.simple.*;
 import org.apache.flink.streaming.api.functions.source.RichSourceFunction;
 import org.cs511.avro.DummyAvro;
 
@@ -8,21 +11,24 @@ import org.cs511.avro.DummyAvro;
 public class DummyAvroDataSource extends RichSourceFunction<DummyAvro> {
     private boolean running = true;
 
-
     @Override
     public void run(SourceContext<DummyAvro> sourceContext) throws Exception {
+        Object datasetObj = new JSONParser().parse(new FileReader("ImdbTitleRatings.json"));
+        JSONArray dataLines = (JSONArray) datasetObj;
 
-        while (running){
-            // parse each line to a pojo
-
-            // insert code for parsing file into records and emit one at a time
-
-
+        Iterator itr = dataLines.iterator();
+        while (itr.hasNext() && this.running){
+            JSONObject dataLine = itr.next();
             DummyAvro avroObj = new DummyAvro();
-            avroObj.setId(1);
-            avroObj.setName("Hello");
-            sourceContext.collect(avroObj);   // emit record
+
+            avroObj.setTconst(dataLine.get("tconst"));
+            avroObj.setAverageRating(dataLine.get("averageRating"));
+            avroObj.setNumVotes(dataLine.get("numVotes"));
+
+            sourceContext.collect(avroObj);
         }
+
+        this.running = false;
     }
 
     @Override
