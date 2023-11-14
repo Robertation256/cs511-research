0bc3a567da6487378e6aa4a91e1845c2c9565519
owenhhj <juanjuanjks@163.com>
2023-11-14 11:03:41 -0600

    Added type cast when reading JSON file. TODO: Add classes to AvroPipeline.java.

--- cs511-research/src/main/java/org/cs511/datasource/DummyAvroDataSourceMedium.java
+++ cs511-research/src/main/java/org/cs511/datasource/DummyAvroDataSourceMedium.java
@@ -3,32 +3,35 @@ package org.cs511.datasource;
 
 import java.io.FileReader;
 import java.util.Iterator;
-import org.json.simple.*;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 import org.apache.flink.streaming.api.functions.source.RichSourceFunction;
 import org.cs511.avro.DummyAvroMedium;
 
 
-public class DummyAvroDataSource extends RichSourceFunction<DummyAvroMedium> {
+public class DummyAvroDataSourceMedium extends RichSourceFunction<DummyAvroMedium> {
     private boolean running = true;
 
     @Override
     public void run(SourceContext<DummyAvroMedium> sourceContext) throws Exception {
-        Object datasetObj = new JSONParser().parse(new FileReader("steam.json"));
+        JSONParser jp = new JSONParser();
+        Object datasetObj = jp.parse(new FileReader("../datasets/steam.json"));
         JSONArray dataLines = (JSONArray) datasetObj;
 
         Iterator itr = dataLines.iterator();
         while (itr.hasNext() && this.running){
-            JSONObject dataLine = itr.next();
+            JSONObject dataLine = (JSONObject) itr.next();
             DummyAvroMedium avroObj = new DummyAvroMedium();  // level 1
 
-            avroObj.setDate(dataLine.get("date"));
-            avroObj.setDeveloper(dataLine.get("developer"));
-            avroObj.setPublisher(dataLine.get("publisher"));
+            avroObj.setDate((String) dataLine.get("date"));
+            avroObj.setDeveloper((String) dataLine.get("developer"));
+            avroObj.setPublisher((String) dataLine.get("publisher"));
 
             DummyAvroMediumFullDesc avroFullDesc = new DummyAvroMediumFullDesc();  // level 2
-            JSONObject dataLineFullDesc = dataLine.get("full_desc");
-            avroFullDesc.setSort(dataLineFullDesc.get("sort"));
-            avroFullDesc.setDesc(dataLineFullDesc.get("desc"));
+            JSONObject dataLineFullDesc = (JSONObject) dataLine.get("full_desc");
+            avroFullDesc.setSort((String) dataLineFullDesc.get("sort"));
+            avroFullDesc.setDesc((String) dataLineFullDesc.get("desc"));
 
             avroObj.setFullDesc(avroFullDesc);
 
